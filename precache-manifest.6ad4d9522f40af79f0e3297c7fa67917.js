3c38348f6d5534a9ecafd31c18d7c080f245c656
J. Graves <jwgravesfl@gmail.com>
2018-11-09 14:43:18 -0700

    Updates

---
+++ precache-manifest.6ad4d9522f40af79f0e3297c7fa67917.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "63ec97fc598e72516e7c401245630606",
+    "url": "/static/media/logo-1024.63ec97fc.png"
+  },
+  {
+    "revision": "963ab3d2bffeff011fcebdcad46b2a11",
+    "url": "/static/media/landingPageMobile.963ab3d2.jpg"
+  },
+  {
+    "revision": "aa2b690f56f76862cc52e9a35a28403f",
+    "url": "/static/media/landingPageDesktop.aa2b690f.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e879cf69b6e28f2efe67",
+    "url": "/static/js/main.e879cf69.chunk.js"
+  },
+  {
+    "revision": "8913fa84862e1cf470c6",
+    "url": "/static/js/1.8913fa84.chunk.js"
+  },
+  {
+    "revision": "8913fa84862e1cf470c6",
+    "url": "/static/css/1.824b34d1.chunk.css"
+  },
+  {
+    "revision": "62d7de01f37e4acbe61656f248572c92",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/SexyMaids/precache-manifest.eef470fdd1f69f821b41f7628493666f.js"
+  "/precache-manifest.6ad4d9522f40af79f0e3297c7fa67917.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/SexyMaids/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
