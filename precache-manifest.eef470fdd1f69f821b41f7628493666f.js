2ad1ef8fd1cda774e088e53d097dbcc3a9e23f75
J. Graves <jwgravesfl@gmail.com>
2018-11-09 14:08:16 -0700

    Updates

---
+++ precache-manifest.eef470fdd1f69f821b41f7628493666f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "63ec97fc598e72516e7c401245630606",
+    "url": "/SexyMaids/static/media/logo-1024.63ec97fc.png"
+  },
+  {
+    "revision": "963ab3d2bffeff011fcebdcad46b2a11",
+    "url": "/SexyMaids/static/media/landingPageMobile.963ab3d2.jpg"
+  },
+  {
+    "revision": "aa2b690f56f76862cc52e9a35a28403f",
+    "url": "/SexyMaids/static/media/landingPageDesktop.aa2b690f.jpg"
+  },
+  {
+    "revision": "7dbd0c193b896373e854",
+    "url": "/SexyMaids/static/js/runtime~main.7dbd0c19.js"
+  },
+  {
+    "revision": "8abf6657f44e60dde887",
+    "url": "/SexyMaids/static/js/main.8abf6657.chunk.js"
+  },
+  {
+    "revision": "473eb7c35e8c0152171d",
+    "url": "/SexyMaids/static/js/1.473eb7c3.chunk.js"
+  },
+  {
+    "revision": "473eb7c35e8c0152171d",
+    "url": "/SexyMaids/static/css/1.824b34d1.chunk.css"
+  },
+  {
+    "revision": "285f46162f5794112733f0f694e7bddc",
+    "url": "/SexyMaids/index.html"
+  }
+];
